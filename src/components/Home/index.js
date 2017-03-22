d7fc419b99bd40289131427385dd187d5fe5bf06
lesik <aleksonufriev@gmail.com>
2017-03-22 22:42:06 +0300

    update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -17,6 +17,7 @@ export default class Home extends Component {
     
     return (
       <div className='row'>
+      <div className='block'>
         <h2>Slider News</h2>
       <hr/>
         <div className='slider'>
@@ -28,66 +29,67 @@ export default class Home extends Component {
           </Slider> 
         </div>
       <hr />
-      <h2>News today</h2>
-      <hr />
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <div className='min1'/>
-          <div className="caption">
-            <h3>UN will hold a meeting</h3>
-            <p>UN will hold a meeting in honor of Vitaly Churkin</p>
-            <p><a href="#" className="btn btn-primary" role="button">Read</a></p>
+        <h2>News today</h2>
+        <hr />
+        <div className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className='min1'/>
+            <div className="caption">
+              <h3>UN will hold a meeting</h3>
+              <p>UN will hold a meeting in honor of Vitaly Churkin</p>
+              <p><a href="#" className="btn btn-primary" role="button">Read</a></p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <div className='min2'/>
-          <div className="caption">
-            <h3>Bloody meat mincers near Kominternovo</h3>
-            <p>The ukraine has lost at least 10 people killed during the failed attack</p>
-            <p><a href="#" className="btn btn-primary" role="button">Read</a></p>
+        <div className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className='min2'/>
+            <div className="caption">
+              <h3>Bloody meat mincers near Kominternovo</h3>
+              <p>The ukraine has lost at least 10 people killed during the failed attack</p>
+              <p><a href="#" className="btn btn-primary" role="button">Read</a></p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <div className='min3'/>
-          <div className="caption">
-            <h3>Battles broke out under Donetsk and Dokuchaevsk</h3>
-            <p>Battles broke out near Donetsk and Dokuchaevsk, Ukraine applied artillery</p>
-            <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+        <div className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className='min3'/>
+            <div className="caption">
+              <h3>Battles broke out under Donetsk and Dokuchaevsk</h3>
+              <p>Battles broke out near Donetsk and Dokuchaevsk, Ukraine applied artillery</p>
+              <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+            </div>
           </div>
         </div>
-      </div>
-      <hr />
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <div className='min4'/>
-          <div className="caption">
-            <h3>The Russian army entered the Afrin</h3>
-            <p>The column of Russian military equipment entered the Kurdish enclave of Afrin in the Syrian province of Aleppo.</p>
-            <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+        <hr />
+        <div className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className='min4'/>
+            <div className="caption">
+              <h3>The Russian army entered the Afrin</h3>
+              <p>The column of Russian military equipment entered the Kurdish enclave of Afrin in the Syrian province of Aleppo.</p>
+              <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <div className='min5'/>
-          <div className="caption">
-            <h3>First sanctions will be removed by the US</h3>
-            <p>Head of Intesa: "First, sanctions will be removed by the US, and then - by Europe"</p>
-            <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+        <div className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className='min5'/>
+            <div className="caption">
+              <h3>First sanctions will be removed by the US</h3>
+              <p>Head of Intesa: "First, sanctions will be removed by the US, and then - by Europe"</p>
+              <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="col-sm-6 col-md-4">
-        <div className="thumbnail">
-          <div className='min6'/>
-          <div className="caption">
-            <h3>Virus in ATMs</h3>
-            <p>Contactless cards in ATMs threatens to lose $ 2 billion</p>
-            <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+        <div className="col-sm-6 col-md-4">
+          <div className="thumbnail">
+            <div className='min6'/>
+            <div className="caption">
+              <h3>Virus in ATMs</h3>
+              <p>Contactless cards in ATMs threatens to lose $ 2 billion</p>
+              <p><a href="#" className="btn btn-primary" role="button">Read</a> </p>
+            </div>
           </div>
         </div>
       </div>
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -7,12 +7,14 @@ export default class App extends Component {
   render() {
     return (
       <div className='container'>
-        <ul className='nav nav-pills'>
-          <li className='main'><NavLink onlyActiveOnIndex={true} to='/'>Main</NavLink></li>
-          <li className='admin'><NavLink to='/admin'>Admin</NavLink></li>
-          <li className='newsList'><NavLink to='/list'>News List</NavLink></li>
-          <li className='login'><NavLink to='/login'>Sign in</NavLink></li>
-        </ul>
+        <div className='heads'>
+          <ul className='nav nav-pills'>
+            <li className='main'><NavLink onlyActiveOnIndex={true} to='/' className='main'>Main</NavLink></li>
+            <li className='admin'><NavLink to='/admin' className='admin'>Admin</NavLink></li>
+            <li className='newsList'><NavLink to='/list' className='newsList'>News List</NavLink></li>
+            <li className='login'><NavLink to='/login' className='login'>Sign in</NavLink></li>
+          </ul>
+        </div>
         {this.props.children}
       </div>
     )
